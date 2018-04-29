1773741c0e11dc71d86d30ce83c63ab55a6a22f9
Brian Abbondana <i3erica@icloud.com>
2018-04-29 17:20:56 -0400

    Removed Function override for touches began.

--- Circularity/GameScene.swift
+++ Circularity/GameScene.swift
@@ -9,3 +9,30 @@
 import SpriteKit
 import GameplayKit
 
+class GameScene: SKScene {
+    
+    var lock = SKShapeNode()
+    var path = UIBezierPath()
+    
+    let zeroAngle: CGFloat = 0.0
+    
+    override func didMove(to view: SKView) {
+        layoutGame()
+    }
+    
+    func layoutGame() {
+        backgroundColor = SKColor.black
+        
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: zeroAngle, endAngle: zeroAngle + CGFloat(Double.pi * 2), clockwise: true)
+        lock = SKShapeNode(path: path.cgPath)
+        lock.strokeColor = SKColor.cyan
+        lock.lineWidth = 40.0
+        self.addChild(lock)
+    }
+    
+   // removed override here for touches began// 
+    
+    override func update(_ currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
--- Circularity/GameViewController.swift
+++ Circularity/GameViewController.swift
@@ -10,3 +10,29 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .aspectFill
+        
+        skView.presentScene(scene)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+}
